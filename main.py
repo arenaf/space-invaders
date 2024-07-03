266a9fa60f2613471a7f9a855b4bfa2f71d7c5d3
arena <arenaferreira@gmail.com>
2024-07-03 12:57:04 +0200

    Create ship

--- main.py
+++ main.py
@@ -14,15 +14,34 @@ screen_width = 800
 screen_height = 600
 screen = pygame.display.set_mode((screen_width, screen_height))
 
+user_ship = ship.create_ship()
+
+shots = []
 run = True
+
 while run:
+
+    screen.fill((0, 0, 0))  # Refresca la ventana y pinta de negro
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
     # Dibuja la nave
-    pygame.draw.rect(screen, ship_bg, ship.create_ship())
+    pygame.draw.rect(screen, ship_bg, user_ship)
+
+    # Manejo de teclas
+    key = pygame.key.get_pressed()
+    if key[pygame.K_RIGHT]:
+        ship.move_right(user_ship)
+    if key[pygame.K_LEFT]:
+        ship.move_left(user_ship)
+    if key[pygame.K_UP]:
+        new_shot = pygame.draw.rect(screen, ship_bg, ship.attack(user_ship))
+        shots.append(new_shot)
+    for shot in shots:
+        shot.move_ip(0, -1)
+
     # Actualiza el lienzo para mostrar los cambios
     pygame.display.update()
 
